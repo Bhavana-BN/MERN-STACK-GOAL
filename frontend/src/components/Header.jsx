54cce90a1ee20e669d17d05259504e1953e2207d
Bhavana <bhavanabn07@gmail.com>
2022-07-05 16:26:29 +0530

    initial commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector,useDispatch} from 'react-redux'
+import {logout,reset} from '../features/auth/authSlice'
+function Header() {
+    const navigate = useNavigate()
+    const dispath = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispath(logout())
+        dispath(reset())
+        navigate("/")
+    }
+  return (
+    <header className='header'>
+        <div className="logo">
+            <Link to='/'>GoalSetter</Link>
+        </div>
+        <ul>
+            {user ? (
+            <li>
+            <button  className='btn' onClick={onLogout}>
+                <FaSignOutAlt />Logout
+                </button>
+        </li>
+            ) : (
+            <>
+            <li>
+                <Link to='/Login'>
+                    <FaSignInAlt />Login
+                </Link>
+            </li>
+            <li>
+                <Link to='/Register'>
+                    <FaUser />Register
+                </Link>
+            </li></>)}
+            
+        </ul>
+    </header>
+  )
+}
+
+export default Header
